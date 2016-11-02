253dc57e78a46e3191db12cef452d13eb0658961
gsegura96 <gsegura96@openmailbox.org>
2016-11-02 15:13:56 -0600

    Creado modal para confirmar orden

--- MonchaClient/www/js/controllers.js
+++ MonchaClient/www/js/controllers.js
@@ -28,14 +28,34 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('menuItemCtrl', ['$scope', '$stateParams', 'MenuService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('menuItemCtrl', ['$scope', '$stateParams', 'MenuService','$ionicModal', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, MenuService) {
+function ($scope, $stateParams, MenuService,$ionicModal) {
 $scope.dish={};
 MenuService.getDish($stateParams.id).then(function(res){
   $scope.dish =res;
 });
+//modal controller
+$scope.getTotal=function(){
+  total=0;
+  if($scope.dish.quantity!=undefined){
+  total= $scope.dish.price * $scope.dish.quantity;
+}
+  return total;
+}
+$ionicModal.fromTemplateUrl('templates/orderModal.html', {
+  scope: $scope,
+  animation: 'slide-in-up'
+}).then(function(modal) {
+  $scope.modal = modal;
+});
+ $scope.openModal = function() {
+   $scope.modal.show();
+ };
+ $scope.closeModal = function() {
+$scope.modal.hide();
+};
 
 }])
 
