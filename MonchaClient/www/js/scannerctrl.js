aeffd79cb2a8a74bb79b59a32b2bf0bd09c45ab3
DanielC <daniel.esteban.8@hotmail.com>
2016-11-05 09:16:48 -0600

    add qr

--- MonchaClient/www/js/scannerctrl.js
+++ MonchaClient/www/js/scannerctrl.js
@@ -1,11 +1,21 @@
 angular.module('lectorctrl.scannerctrl', ['ionic','ngCordova'])
-  .controller("lectorctrl.scannerctrl", function($scope,$cordovaBarcodeScanner ){
+  .controller("lectorctrl.scannerctrl", ['$state', '$scope','$cordovaBarcodeScanner','userData', function($state, $scope, $cordovaBarcodeScanner, userData ){
       $scope.leer= function(){
         $cordovaBarcodeScanner.scan().then ( function(imagenEscaneada){
 
           //imagenEscaneada.text es un string con la informacion del QR
-          alert(imagenEscaneada.text)
+          data = JSON.parse(imagenEscaneada.text);
 
+          if (data.mesa && data.tipo) {
+            user = {"name": "Cliente", "table": data.mesa, "type": data.tipo};
+            userData.updateUser("Cliente", data.mesa, data.tipo);
+            userData.login(this.user).then(function(res){
+              userData.setToken(res);
+              $state.go('tabsController.desserts_tab4');
+            });
+          } else {
+            alert("The QR is invalid");
+          }
 
 
   			}, function(error){
@@ -13,4 +23,4 @@ angular.module('lectorctrl.scannerctrl', ['ionic','ngCordova'])
   			});
       }
 
-  } );
+  } ]);
